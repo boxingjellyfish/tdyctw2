07b9acd7dfc3797a0568fc85646715696fbbeb69
boxingjellyfish <boxingjellyfish@noreply.com>
2017-05-02 21:29:26 -0300

    Identity

---
+++ tdyctw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tdyctw.Startup))]
+namespace tdyctw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
